e03b98ffd5cf6657152e5040949245b5e6d28d11
Inkyu-Kim <sda1547@gmail.com>
2021-02-06 12:59:12 +0900

    add login auth

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,20 +12,24 @@ from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
 
-        temp = request.POST.get('hello_world_input')
+        if request.method == 'POST':
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        # 새로고침 해도 다시 POST 값이 재요청되지 않고 한 번에 그치게 하는 방법
-        ## reverse는 함수형에서 사용
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            temp = request.POST.get('hello_world_input')
 
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+            # 새로고침 해도 다시 POST 값이 재요청되지 않고 한 번에 그치게 하는 방법
+            ## reverse는 함수형에서 사용
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
