073178f7121f94fcf28f4081f279a52ef24d7467
Inkyu-Kim <sda1547@gmail.com>
2021-02-14 21:35:38 +0900

    split serttings to local/deploy

--- Pinterest/settings/base.py
+++ Pinterest/settings/base.py
@@ -16,28 +16,8 @@ import os, environ
 
 from django.urls import reverse_lazy
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(
-    env_file = os.path.join(BASE_DIR, '.env')
-)
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Application definition
 
@@ -88,17 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Pinterest.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -137,6 +106,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# python manage.py collectstatic 명령 시 아래 경로로 static 파일들이 생성
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
