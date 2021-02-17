eef6d4db958b03a4c910b2e8506f76f72b1dabba
Inkyu-Kim <sda1547@gmail.com>
2021-02-17 23:17:10 +0900

    edit AccountCreateView

--- accountapp/views.py
+++ accountapp/views.py
@@ -20,7 +20,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')  # reverse_lazy는 클래스형에서 사용
+    success_url = reverse_lazy('articleapp:list')  # reverse_lazy는 클래스형에서 사용
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
