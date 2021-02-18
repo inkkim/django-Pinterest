af01e3cc842e093db126922338352c1a6ad57051
Inkyu-Kim <sda1547@gmail.com>
2021-02-18 23:14:45 +0900

    add swarm secret

--- Pinterest/settings/deploy.py
+++ Pinterest/settings/deploy.py
@@ -1,5 +1,12 @@
 from .base import *
 
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -12,7 +19,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -27,7 +34,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('DJANGO_SECRET_KEY'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
